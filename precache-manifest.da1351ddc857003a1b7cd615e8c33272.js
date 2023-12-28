0cfc0cf5154b1954d266d276b324e7e83063ddce
nvhlong2910 <nvhlong2910@gmail.com>
2023-12-29 02:13:51 +0700

    Updates

---
+++ precache-manifest.da1351ddc857003a1b7cd615e8c33272.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7b937fb0d6584ac92a6165930d253dd",
+    "url": "/ecommerce-client/index.html"
+  },
+  {
+    "revision": "754fd2721dd426337b30",
+    "url": "/ecommerce-client/static/css/2.79df26c8.chunk.css"
+  },
+  {
+    "revision": "cd6443af5dece42bcbde",
+    "url": "/ecommerce-client/static/css/main.8b93b7df.chunk.css"
+  },
+  {
+    "revision": "754fd2721dd426337b30",
+    "url": "/ecommerce-client/static/js/2.0be64959.chunk.js"
+  },
+  {
+    "revision": "cd6443af5dece42bcbde",
+    "url": "/ecommerce-client/static/js/main.2cc1f118.chunk.js"
+  },
+  {
+    "revision": "cc08fa1b06f85e84c9d6",
+    "url": "/ecommerce-client/static/js/runtime-main.01786dd4.js"
+  },
+  {
+    "revision": "48e62dcdd413f4e4b8430b74ae694743",
+    "url": "/ecommerce-client/static/media/ip-01.48e62dcd.jpg"
+  },
+  {
+    "revision": "b74f6273c4e3c0e307b326faf86b8aa9",
+    "url": "/ecommerce-client/static/media/ipad1-01.b74f6273.jpg"
+  },
+  {
+    "revision": "11f28441ea9bcdbbde516d04affb1f2c",
+    "url": "/ecommerce-client/static/media/laptop-01.11f28441.jpg"
+  },
+  {
+    "revision": "7db625fb125829b6f51496a2441fea5f",
+    "url": "/ecommerce-client/static/media/mb-01.7db625fb.jpg"
+  },
+  {
+    "revision": "c8f2cd0abb342fb3d1b255b6f96c343f",
+    "url": "/ecommerce-client/static/media/msi-01.c8f2cd0a.jpg"
+  },
+  {
+    "revision": "f9592dcbaa1862ccd10e26644855b77e",
+    "url": "/ecommerce-client/static/media/phone 2-01.f9592dcb.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/ecommerce-client/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/ecommerce-client/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/ecommerce-client/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/ecommerce-client/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "b0627be9cd31cd7e1ef3d9ecfba5cfb3",
+    "url": "/ecommerce-client/static/media/slide-02.b0627be9.jpg"
+  },
+  {
+    "revision": "416d7122dafb6b8cf2949e67b39e6686",
+    "url": "/ecommerce-client/static/media/tablet 2-01.416d7122.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b9d27012ddc6df13bed1c22892255ea2.js"
+  "/ecommerce-client/precache-manifest.da1351ddc857003a1b7cd615e8c33272.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ecommerce-client/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
